5043b1614c033e07f7ad471135c8ef921a3764de
Dong SHIN <dong_s@me.com>
2022-04-25 01:11:05 +0900

    feat(game): combine each class and logics
    change to Constants to final

--- src/main/java/racingcar/RacingGameConstants.java
+++ src/main/java/racingcar/RacingGameConstants.java
@@ -2,8 +2,8 @@ package racingcar;
 
 public class RacingGameConstants {
 
-    public static int MAX_NAME_LENGTH = 5;
-    public static int MOVING_CONDITION = 4;
-    public static int RANDOM_LOWER_BOUND = 0;
-    public static int RANDOM_UPPER_BOUND = 9;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MOVING_CONDITION = 4;
+    public static final int RANDOM_LOWER_BOUND = 0;
+    public static final int RANDOM_UPPER_BOUND = 9;
 }
--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,8 +1,37 @@
 package racingcar.controller;
 
-public class RacingGame {
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.view.Prompt;
 
-    public void run_game() {
+public class RacingGame {
+    private final Prompt prompt;
+    private final RacingGameLogic logic;
+    public RacingGame() {
+        prompt = Prompt.getInstance();
+        logic = RacingGameLogic.getInstance();
     }
-    // TODO: LOGIC, Prompt, Car Model
+    public void run_game() {
+        List<Car> cars = getCars();
+        int loops = getLoopCount();
+        for (int i = 0; i < loops; i++) {
+            logic.moveCars(cars);
+            prompt.printInterimResults(cars);
+        }
+        prompt.printFinalResults(logic.selectWinnerName(cars));
+    }
+
+    private int getLoopCount() {
+        return prompt.getGameCount();
+    }
+
+    private List<Car> getCars() {
+        while(true) {
+            try {
+                return logic.parserCarName(prompt.getCarNames());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
