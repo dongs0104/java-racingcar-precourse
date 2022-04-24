4d480a9356370e6b71a8961a295d766914c84d36
Dong SHIN <dong_s@me.com>
2022-04-24 21:22:54 +0900

    feat: add constants and message formeat

---
+++ src/main/java/racingcar/RacingGameConstants.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class RacingGameConstants {
+
+    public static int MAX_NAME_LENGTH = 5;
+    public static int MOVING_CONDITION = 4;
+    public static int RANDOM_LOWER_BOUND = 0;
+    public static int RANDOM_UPPER_BOUND = 9;
+}
