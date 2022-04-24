88e908b2d4407e0acc8d7334866b2790623840c9
Dong SHIN <dong_s@me.com>
2022-04-25 01:07:50 +0900

    feat(model): add car test case and move function to set method

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -9,10 +9,6 @@ public class Car {
     private int moveCount = 0;
 
     public Car(String name) {
-        if (name.length() > RacingGameConstants.MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException(
-                "[ERROR] 자동차의 이름은 " + RacingGameConstants.MAX_NAME_LENGTH + "자를 넘을 수 없습니다.");
-        }
         setName(name);
     }
 
@@ -22,10 +18,22 @@ public class Car {
 
 
     public void setName(String name) {
+        if (name.length() > RacingGameConstants.MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(
+                "[ERROR] 자동차의 이름은 " + RacingGameConstants.MAX_NAME_LENGTH + "자를 넘을 수 없습니다.");
+        }
         this.name = name;
     }
 
     public void setMoveCount(int count) {
+        if (!(this.moveCount -1 <= count && count <= this.moveCount + 1)) {
+            throw new IllegalArgumentException(
+                "[ERROR] 한번에 두 칸 이상 움직일 수 없습니다.");
+        }
+        if (count < 0) {
+            throw new IllegalArgumentException(
+                "[ERROR] 뒤로 갈 수 없습니다.");
+        }
         this.moveCount = count;
     }
 
